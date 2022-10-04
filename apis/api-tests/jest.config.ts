f33931fe00401add4ddd3d596eb9ecdb1a3856a7
Carlos Aquino <carlos.aquino.ext@infracommerce.com.br>
2022-10-04 07:58:17 -0300

    postgre and postgre admin

---
+++ apis/api-tests/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: '..-apis-api-tests',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apis/api-tests',
+};
